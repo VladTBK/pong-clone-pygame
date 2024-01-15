057bae00b0a1bd779eebd998e34ac6ba2381e27c
VladTBK <most_loginVlad.TBK@proton.me>
2024-01-15 15:25:23 +0200

    made ball bounce

--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
 class cfg:
     color_dict = {0: "red", 1: "black"}
-    height = 1280
-    width = 720
+    height = 720
+    width = 1280
+    ballspeed = 5
--- pong.py
+++ pong.py
@@ -7,42 +7,50 @@ def check_key(key):
         pygame.quit()
 
 
+def refresh(check_run):
+    pygame.time.delay(10)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            check_run = False
+    screen.fill(cfg.color_dict[1])
+    return check_run
+
+
 class Ball:
-    def __init__(self, color, pos_X, pos_Y):
+    def __init__(self, color, pos_X, pos_Y, size):
         self.color = color
         self.pos_X = pos_X
         self.pos_Y = pos_Y
-        pygame.draw.circle(screen, self.color, (self.pos_X, self.pos_Y), 40)
+        self.vx = cfg.ballspeed
+        self.vy = cfg.ballspeed
+        self.size = size
+        pygame.draw.circle(screen, self.color, (self.pos_X, self.pos_Y), self.size)
 
-    def fall(self):
-        self.pos_Y += 1
-        screen.fill((0, 0, 0))
-        pygame.draw.circle(screen, self.color, (self.pos_X, self.pos_Y), 40)
+    def update_state(self):
+        self.pos_X += self.vx
+        self.pos_Y += self.vy
+        if self.pos_X >= cfg.width - self.size / 2 or self.pos_X <= self.size / 2:
+            self.vx = -self.vx
+
+        if self.pos_Y <= self.size / 2 or self.pos_Y >= cfg.height - self.size / 2:
+            self.vy = -self.vy
+
+        pygame.draw.circle(screen, self.color, (self.pos_X, self.pos_Y), self.size)
 
 
 pygame.init()
-screen = pygame.display.set_mode((cfg.height, cfg.width), vsync=1)
+screen = pygame.display.set_mode((cfg.width, cfg.height), vsync=1)
 clock = pygame.time.Clock()
-
-running = True
-screen.fill(cfg.color_dict[1])
 
 
 def main():
-    global running
-    ball = Ball(cfg.color_dict[0], cfg.height / 2, cfg.width / 2)
+    running = True
+    ball = Ball(cfg.color_dict[0], cfg.width / 2, cfg.height / 2, 20)
     while running:
-        pygame.time.delay(10)
-        # poll for events
-        # pygame.QUIT event means the user clicked X to close your window
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
+        running = refresh(running)
         curr_key = pygame.key.get_pressed()
         check_key(curr_key)
-        ball.fall()
-        # update_ball()
+        ball.update_state()
         pygame.display.flip()
         clock.tick(60) / 1000
 
